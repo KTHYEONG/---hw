bae7d0079d87362e65661aff26ac80c91dce0a9e
xogud <xogud3624@gmail.com>
2023-11-23 23:31:56 +0900

    image, label 입력 해결

--- result/verilog/Top.v
+++ result/verilog/Top.v
@@ -2,53 +2,91 @@
 
 module Top();
 
-reg CLK, nRST, START, X, Y;
+reg CLK, nRST, START;
+reg[4:0] X, Y;
 reg[199:0] IMGIN;
 
 wire DONE;
 wire[3:0] OUT;
 
-simpleCNN sCNN(.CLK(CLK), .nRST(nRST), .START(START), .X(X), .Y(Y), .IMGIN());
+simpleCNN sCNN(.CLK(CLK), .nRST(nRST), .START(START), .X(X), .Y(Y), .IMGIN(IMGIN), .DONE(DONE), .OUT(OUT));
 
 always
     #5 CLK = ~CLK;
 
 initial
 begin
-    CLK = 0;
-    #15 START = 1;
-    #5 START = 0;
+    CLK = 0; X = 0; Y = 0;
+    #30 START = 1;
+    #10 START = 0;
 end
     
-
 initial
 begin
     nRST = 0; START = 0;
-    $monitor("X = %d, Y = %d, DONE = %d, OUT = %d", X, Y, DONE, OUT);
-    #10 nRST = 1;
+    #20 nRST = 1;
 end
 
-// image입력(값 제대로 읽어들이는지 확인)
-initial
+// image 입력
+integer fd;
+always@(START)
 begin
-    $readmemh("image.mem", IMGIN);
-    $display("Image data:");
-    for (int i = 0; i < 200; i = i + 1)
-        $write("%h ", IMGIN[i]);
-    $write("\n");
-end
-
-/*integer i;
-initial
-begin
-    for (i = 0; i < 200; i = i + 1)
+    if (START == 1)
     begin
-        $write("%b ", IMGIN[i]);
+        fd = $fopen("image.mem", "r");
+        if (fd == 0)
+        begin
+            $display("Error: file cannot opend");
+            $finish();
+        end
+        
+        while(!($feof(fd))) begin
+            #10 $fscanf(fd, "%50h", IMGIN);
+            $display("image: %h", IMGIN);
+            Y = Y + 1;
+            if (Y == 29)
+            begin
+                X = X + 1;
+                Y = 0;
+            end
+        end
+        $fclose(fd);
     end
-    $write("\n");
-end*/
+end
 
+// label 입력
+reg[3:0] label[99:0];
+integer fr, i;
+initial
+begin
+    i = 0;
+    fr = $fopen("label.mem", "r");
+    while (!($feof(fr)))
+    begin
+        $fscanf(fr, "%d", label[i]);
+        i = i + 1;
+    end
+    i = 0;
+    $fclose(fr);
+end
 
-// label입력
+// 정확도 체크
+integer err;
+initial err = 0;
+always @(DONE)
+begin
+    if (DONE == 1)
+    begin
+        if (label[i] != OUT)
+            err = err + 1;
+        i = i + 1;
+    end
+end
+
+initial
+begin
+    //#2000 $display("Accuracy: %d%%\n", (100 - err) / 100 * 100); 
+    #3000 $finish;
+end
 
 endmodule
--- result/verilog/simpleCNN.v
+++ result/verilog/simpleCNN.v
@@ -12,21 +12,26 @@ module simpleCNN (
     OUT
 );
 
-input           CLK;
-input           nRST;
-input           START;
-input           X;
-input           Y;
-input   [199:0] IMGIN;
+input   wire        CLK;
+input   wire        nRST;
+input   wire        START;
+input   wire[4:0]       X;
+input   wire[4:0]       Y;
+input   wire[199:0] IMGIN;
     
-output          DONE;
-output  [3:0]   OUT;
+output  reg        DONE;
+output  reg[3:0]   OUT;
 
-// convolution 및 fc의 끝나는 조건을 체크하기 위한 cnt
-reg [10:0] conv_cnt;
-reg [10:0] fc_cnt;
+`include "kernel.vh"
 
-reg signed [7:0] conv_res, fc_res, image_pixel;
+// conv, fc의 결과 저장 변수
+reg[7:0] conv_res[24:0];
+
+reg signed [7:0] conv_num, fc_num;
+integer i, j, sum;
+
+reg[1:0] state;
+parameter IDLE = 2'b00, CONVOLUTION = 2'b01, FULLY_CONNECTED = 2'b10;
 
 // state에 따라 동작    
 always @(posedge CLK or nRST) 
@@ -34,10 +39,8 @@ begin
     if (!nRST)
         begin
             DONE <= 0;
-            OUT <= 4'b0;
+            OUT <= 0;
             state <= IDLE;
-            conv_cnt <= 11'b0;
-            fc_cnt <= 11'b0;
         end
     else
         begin
@@ -46,38 +49,36 @@ begin
                 begin
                     if (START)
                     begin
+                        DONE <= 0;
                         state <= CONVOLUTION;
-                        conv_cnt <= 11'b0;
                     end
                 end
 
+                // x=0,y=0 x=0,y=1 ... 각각 200bit --> 총 24 * 24 번 conv 수행
                 CONVOLUTION:
-                begin 
-                    image_pixel <= IMGIN[X + Y * 5];
-                    conv_res <= conv_kernel(X, Y);
-                    conv_cnt <= conv_cnt + 1;
-
-                    // convolution이 끝나면 state -> fc로 변환
-                    if (conv_cnt == 576)
-                    begin 
-                        state <= FULLY_CONNECTED;
-                        fc_cnt <= 11'b0;
+                begin
+                    sum = 0;
+                    for (i = 0; i < 5; i = i + 1)
+                    begin
+                        for (j = 0; j < 5; j = j + 1)
+                        begin
+                            conv_num <= conv_kernel(i, j);
+                            sum <= sum + conv_num * IMGIN[i * 5 + j];
+                        end
                     end
+
+                    // conv 끝난 후 fc 수행
+                    state <= FULLY_CONNECTED;
                 end
 
                 FULLY_CONNECTED:
                 begin
-                    fc_res <= fc_kernel(Y, conv_res);
-                    fc_cnt <= fc_cnt + 1;
-                    
-                    if (fc_cnt == 576)
-                    begin
-                        DONE <= 1;
-                        OUT <= fc_res[3:0];
-                        state <= IDLE;
-                    end
+                    fc_num <= fc_kernel(Y, conv_num);
+                
+                    DONE <= 1;
+                    //OUT <= fc_res[3:0];
+                    state <= IDLE;
                 end
-
             endcase
         end
 end
