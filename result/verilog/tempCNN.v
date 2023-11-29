32c81db98d6bdef72b02ba4d3013bbbb70076de4
xogud <xogud3624@gmail.com>
2023-11-30 00:44:16 +0900

    각 단계마다 값이 제대로 저장되는지 확인하기

--- result/verilog/tempCNN.v
+++ result/verilog/tempCNN.v
@@ -1,6 +1,5 @@
 `timescale 1ns / 1ps
 
-// accuracy 9% 나오는 문제 해결하기(IMGIN 값 저장 알고리즘 or conv, fc 알고리즘)
 module simpleCNN (
         CLK,
         nRST,
@@ -26,23 +25,22 @@ module simpleCNN (
     `include "kernel.vh"
 
     reg[1:0] state;
-    parameter IDLE = 2'b00, CONVOLUTION = 2'b01, RELU = 2'b10, FULLY_CONNECTED = 2'b11;
+    parameter IDLE = 2'b00, CONVOLUTION = 2'b01,
+            FULLY_CONNECTED = 2'b10, RESULT = 2'b11;
 
-    // CONVOLUTION에서 사용하는 변수
-    reg signed [15:0] conv_res[575:0];
-    reg signed [15:0] mul_res;
-    reg signed [15:0] sum;
-    integer conv_cnt;
+    // CONVOLUTION && RELU
+    reg signed [399:0] conv_res [23:0][23:0];
+    reg signed [399:0] conv_sum;
 
-    // FC에서 사용하는 변수
-    reg signed [23:0] fc_res[9:0];
-    integer ans_idx;
+    // FC
+    reg signed [599:0] fc_res [9:0];
+    reg signed [599:0] fc_sum;
+    integer max_num, ans_idx;
 
     integer i, j;
-    always @(posedge CLK or negedge nRST)
+    always @(posedge CLK)
     begin
         if (!nRST) begin
-            conv_cnt <= 0;
             DONE <= 0;
             OUT <= 0;
             state <= IDLE;
@@ -50,62 +48,57 @@ module simpleCNN (
         else begin
             case (state)
                 IDLE: begin
-                    conv_cnt <= 0;
-                    DONE <= 0;
                     OUT <= 0;
-                    if (START) begin
-                        sum <= 0;
+                    DONE <= 0;
+                    if (START)
                         state <= CONVOLUTION;
-                    end
                 end
                 
                 CONVOLUTION: begin
-                    $display("x: %d, y: %d", X, Y);
-                    for (i = 0; i < 5; i = i + 1) begin
-                        for (j = 0; j < 5; j = j + 1) begin
-                            sum <= sum + conv_kernel(i, j) * IMGIN[(i * 5 + j) * 8 +: 8];
-                            //$display("%d sum: %h, IMGIN: %h", conv_cnt, sum, IMGIN[(i * 5 + j) * 8 +: 8]);
+                    // convolution && relu
+                    for (i = 0; i < 24; i = i + 1) begin
+                        for (j = 0; j < 24; j = j + 1) begin
+                            for (integer k = 0; k < 5; k = k + 1) begin
+                                for (integer l = 0; l < 5; l = l + 1) begin
+                                    if (conv_kernel(k, l) * IMGIN[(k * 5 + l) * 8 +: 8] < 0)
+                                        conv_sum[(k * 5 + l) * 16 +: 16] <= 0;
+                                    else
+                                        conv_sum[(k * 5 + l) * 16 +: 16] <= conv_kernel(k, l) * IMGIN[(k * 5 + l) * 8 +: 8];
+                                    $display("sum: %h", IMGIN[(k * 5 + l) * 8 +: 8]);
+                                end
+                            end
+                            conv_res[i][j] <= conv_sum;
                         end
                     end
-                    #1 conv_res[conv_cnt] <= sum;
-                    #1 state <= RELU;
 
-                    //$display("conv_cnt: %d, conv_res/sum: %h/%h", conv_cnt, conv_res[conv_cnt], sum);
-                end
+                    for (integer i = 0; i < 10; i = i + 1)
+                        fc_res[i] <= 0;
 
-                RELU: begin
-                    if (sum < 0)
-                        conv_res[conv_cnt] <= 0;
-
-                //$display("conv_res: %h", conv_res[conv_cnt]);
-
-                #1 conv_cnt <= conv_cnt + 1;
-                #1 state <= FULLY_CONNECTED;
-
+                    state <= FULLY_CONNECTED;
                 end
 
                 FULLY_CONNECTED: begin
-                    for (i = 0; i < 10; i = i + 1) begin
-                        fc_res[i] = 0;
-                        for (j = 0; j < 576; j = j + 1) begin
-                            fc_res[i] = fc_res[i] + fc_kernel(i, j) * conv_res[j];
-                            //$display("fc_res: %h, fc_kernel: %h, conv_res: %h", fc_res[i], fc_kernel(i, j), conv_res[j]);
+                    for (integer i = 0; i < 10; i = i + 1) begin
+                        for (integer j = 0; j < 24 * 24; j = j + 1) begin
+                            fc_sum[(i * 5 + j) * 24 +: 24] <= fc_kernel(i, j) * conv_res[j / 24][j % 24];
                         end
-                        //$display("fc_res[%d]: %d", i, fc_res[i]);
-                    end
-                    
-                    // OUT 결정
-                    ans_idx = 0;
-                    for (i = 1; i < 10; i = i + 1) begin
-                        //$display("fc_res[i]: %d, fc_res[ans]: %d, ans_idx: %d", fc_res[i], fc_res[ans_idx], ans_idx);
-                        if (fc_res[i] > fc_res[ans_idx])
-                            ans_idx = i;
+                        fc_res[i] <= fc_sum;
+
+                        if (fc_res[i] > max_num) begin
+                            max_num <= fc_res[i];
+                            ans_idx <= i;
+                        end
                     end
 
-                    OUT <= ans_idx;
-                    state <= IDLE;
-                    DONE <= 1;
+                    state <= RESULT;
                 end
+                
+                RESULT: begin
+                    OUT <= ans_idx;
+                    DONE <= 1;
+                    state <= IDLE;
+                end
+
             endcase
         end
     end
